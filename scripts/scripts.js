30f692b8820cc9514d9f8de95389edced9afba5d
sueng0422 <sueng0422@naver.com>
2024-02-18 22:41:02 +0900

    css 수정 및 스크립트 추가

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,24 @@
 var navBar = $("#nav_bar");
-    var navBtnShow = $("i.fas.fa-ellipsis");
-    var navBtnhide = $("i.fas.fa-xmark");
+var navBtnShow = $("i.fas.fa-ellipsis");
+var navBtnhide = $("i.fas.fa-xmark");
 
 
-    // media query 시 작동되는 메뉴 이벤트
-    navBtnShow.click(function(){
-        navBar.css({"right":"0"});
-        console.log("menu show");
-    });
+// media query 시 작동되는 메뉴 이벤트
+navBtnShow.click(function () {
+    navBar.css({ "right": "0" });
+    console.log("menu show");
+});
 
-    navBtnhide.click(function(){
-        navBar.css({"right":"-200px"});
-        console.log("menu hide");
-    });
+navBtnhide.click(function () {
+    navBar.css({ "right": "-200px" });
+    console.log("menu hide");
+});
+
+// 클릭 시 부드러운 이동
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
